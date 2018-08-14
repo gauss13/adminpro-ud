4f3f71e69087faf744ab36725ed8c3e9e63c69fd
Edwin <ingedwinhm13@gmail.com>
2018-08-13 19:12:25 -0500

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,20 +7,20 @@ import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
 import { PagesComponent} from './pages/pages.component';
 import {RegisterComponent} from './login/register.component';
 
-const appRoutes:Routes = [
-    {
-        path:'',
-        component:PagesComponent,
-        children:[
-            {path:'dashboard',component:DashboardComponent},
-            {path:'progress',component:ProgressComponent},
-            {path:'graficas1',component:Graficas1Component},
-            {path:'',redirectTo:'/dashboard', pathMatch:'full'}
-        ]
-    },   
-    {path:'login',component:LoginComponent},
-    {path:'register',component:RegisterComponent},
-    {path:'**',component:NopagefoundComponent}
+const appRoutes: Routes = [
+    // {
+    //     path:'',
+    //     component:PagesComponent,
+    //     children:[
+    //         {path:'dashboard',component:DashboardComponent},
+    //         {path:'progress',component:ProgressComponent},
+    //         {path:'graficas1',component:Graficas1Component},
+    //         {path:'',redirectTo:'/dashboard', pathMatch:'full'}
+    //     ]
+    // },
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: '**', component: NopagefoundComponent}
 ];
 
-export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash:true});
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true} );
